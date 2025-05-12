4c7b9402f6c5fe41ff794993699d15b994b6de8c
Henry Letting <lettinghenry0@gmail.com>
2025-05-13 00:32:16 +0300

    added search, card and sectional composables

--- app/src/main/java/com/hyperforge/androidcompose/MainActivity.kt
+++ app/src/main/java/com/hyperforge/androidcompose/MainActivity.kt
@@ -4,14 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.hyperforge.androidcompose.ui.theme.AndroidComposeTheme
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ContentScale.Companion.Crop
+import androidx.compose.ui.layout.ContentScale.Companion.FillBounds
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +44,123 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SearchBar()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun FavoriteCollectionCard(
+    modifier: Modifier = Modifier,
+    image: Int,
+    text: Int
+) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        color = MaterialTheme.colorScheme.surfaceVariant,
         modifier = modifier
-    )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(width = 255.dp)
+        ) {
+            Image(
+                painter = painterResource(image),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = modifier.size(80.dp)
+            )
+            Text(
+                text = stringResource(text),
+                style = MaterialTheme.typography.titleMedium,
+                modifier = modifier.padding(start = 16.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun FavoriteCollectionCardPreview() {
     AndroidComposeTheme {
-        Greeting("Android")
+        FavoriteCollectionCard(
+            image = R.drawable.leaves2,
+            text = R.string.nature_meditations,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.search))
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+    )
+}
+
+@Preview(showBackground = false)
+@Composable
+fun SearchBarPreview() {
+    AndroidComposeTheme {
+        SearchBar()
+    }
+}
+
+@Composable
+fun AlignYourBodyElement(
+    modifier: Modifier = Modifier,
+    imageIntResource: Int,
+    textLabelIntResource: Int
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(imageIntResource),
+            contentDescription = null,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape),
+            contentScale = Crop
+        )
+        Text(
+            text = stringResource(textLabelIntResource),
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp)
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+fun AlignYourBodyElementPreview() {
+    AndroidComposeTheme {
+        AlignYourBodyElement(
+            modifier = Modifier.padding(8.dp),
+            textLabelIntResource = R.string.inversions,
+            imageIntResource = R.drawable.inversions
+        )
     }
 }
